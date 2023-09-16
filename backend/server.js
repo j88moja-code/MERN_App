88e85ba7e623a95dfa3cd2fa9aba99fa4d451c94
j88moja-code <sitholent+github@gmail.com>
2023-09-16 22:28:23 +0200

    add product functionality to the project

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
-const dotenv = require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const userRoute = require('./routes/userRoute');
-const errorHandler = require('./middleWare/errorMiddleware');
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const userRoute = require("./routes/userRoute");
+const productRoute = require("./routes/productRoute");
+const errorHandler = require("./middleWare/errorMiddleware");
 
 const app = express();
 
@@ -16,13 +18,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+// Flie upload to
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
 // Routes Middleware
-app.use('/api/users', userRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
 
 // Routes
 
 app.get("/", (req, res) => {
-    res.send("Home Page");
+  res.send("Home Page");
 });
 
 // Error Middleware
@@ -31,10 +37,10 @@ app.use(errorHandler);
 // Connect to DB and start server
 const PORT = process.env.PORT || 5000;
 mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => {
-        app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`);
-        });
-    })
-    .catch((err) => console.log(err))
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
